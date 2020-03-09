fbfb466cbfbfd61f0cfb40f7cc12c306f3773639
r5racker <rahilsrb@gmail.com>
2020-03-09 22:05:24 +0530

    no comments

--- DjangoAssignmentHandler/RegistrationModule/admin.py
+++ DjangoAssignmentHandler/RegistrationModule/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from RegistrationModule.models import Student,Teacher,Courses,AppUsers,StudentCourse
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Courses)
+admin.site.register(AppUsers)
+admin.site.register(StudentCourse)
--- DjangoAssignmentHandler/manageAssignments/urls.py
+++ DjangoAssignmentHandler/manageAssignments/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    
-    
+    path('StudentHomePage.html',views.displayStudentAssignmentList,name='student_home')
 ]
