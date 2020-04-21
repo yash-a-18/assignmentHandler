20950ad7640c4ded96f5420a520ca764e09334b2
AasthaPatel27 <patelaastha27@gmail.com>
2020-04-21 22:17:25 +0530

    forgot Password implemented

--- DjangoAssignmentHandler/DjangoAssignmentHandler/settings.py
+++ DjangoAssignmentHandler/DjangoAssignmentHandler/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/') #added slash
 
 MEDIA_URL= '/media/'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'patelaastha27@^mail.com'
+EMAIL_HOST_PASSWORD = 'Aasth@270600'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
